97fc97a1c494045d07263926d97e50e2b57fc2fd
natkaf <nataljaf00@gmail.com>
2023-10-03 15:09:19 +0100

    created new html and js files

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {//+=operator function
+   var sentence = "Spring is the most";//creating variable
+   sentence += " beautiful season of the year.";//adding to the variable
+   document.getElementById("Concatenate").innerHTML = sentence;//using document.getElementById
+}

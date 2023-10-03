23a385a5841201e0e7317e120a605797e48feff2
natkaf <nataljaf00@gmail.com>
2023-10-03 13:20:46 +0100

    created html and js files

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+window.alert("Hello, World!");//Window alert method
+document.write('Hello, world!');//Document write method
+var A = "This is a string";
+document.write(A);
+var B = "This is a string for my assingnment!";
+window.alert(B);//Window alert method
+document.write("John came to the office and said \"Good morning everyone!\".");//Document write method
+document.write("\"All of our dreams can come true,"
++"if we have the courage to pursue them.\""
++"-Walt Disney");
+var C = "concantenated"+"string";
+document.write(C);
+var Family = "Smith", Dad = "John", Mom = "Jane", Son = "Billy", Daughter = "Jessica";//Defining variables and giving them string values
+document.write(Daughter);//Printing the assigned value of the daughter variable on the screen
+document.write(5+9);//Adding two values and printing the outcome on the screen
+var Sent1 = "This is the beginning of the string";//Creating a variable
+var Sent2 = " and this is the end of the string";//Creating a variable
+document.write(Sent1 + Sent2);//Displaying two variables using the document.write method
